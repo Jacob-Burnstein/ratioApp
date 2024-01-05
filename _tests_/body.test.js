09d65170ffa0b38a26cc26a0af1062ba64d60b32
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-05 16:13:31 -0500

    wrote unit tests for missing inputs

--- _tests_/body.test.js
+++ _tests_/body.test.js
@@ -5,18 +5,38 @@ import {
   calculateMissingD,
 } from "../src/calculations";
 
-test("calculateMissingA determines the missing proportional value", () => {
-  expect(calculateMissingA(4, 12, 16)).toBe("3.00");
+describe("calculateMissingA", () => {
+  test("determines the missing proportional value", () => {
+    expect(calculateMissingA(4, 12, 16)).toBe("3.00");
+  });
+  test("returns NaN if b, c or d are invalid", () => {
+    expect(calculateMissingA(4, 16)).toBe("NaN");
+  });
 });
 
-test("calculateMissingB determines the missing proportional value", () => {
-  expect(calculateMissingA(3, 6, 12)).toBe("6.00");
+describe("calculateMissingB", () => {
+  test("determines the missing proportional value", () => {
+    expect(calculateMissingB(3, 6, 12)).toBe("6.00");
+  });
+  test("returns NaN if a, c or d are invalid", () => {
+    expect(calculateMissingA(32, 1)).toBe("NaN");
+  });
 });
 
-test("calculateMissingC determines the missing proportional value", () => {
-  expect(calculateMissingA(7, 12, 16)).toBe("9.33");
+describe("calculateMissingC", () => {
+  test("determines the missing proportional value", () => {
+    expect(calculateMissingC(7, 12, 16)).toBe("9.33");
+  });
+  test("returns NaN if a, b or d are invalid", () => {
+    expect(calculateMissingA(4, 17)).toBe("NaN");
+  });
 });
 
-test("calculateMissingD determines the missing proportional value", () => {
-  expect(calculateMissingA(3.2, 4.53, 16)).toBe("22.65");
+describe("calculateMissingD", () => {
+  test("determines the missing proportional value", () => {
+    expect(calculateMissingD(3.2, 4.53, 16)).toBe("22.65");
+  });
+  test("returns NaN if a, b, or c are invalid", () => {
+    expect(calculateMissingA(12, 13)).toBe("NaN");
+  });
 });
