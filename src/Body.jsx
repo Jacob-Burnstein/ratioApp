6daffd544a9741e7e8ecd02687511ed0f414bc8e
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-05 14:23:53 -0500

    changed variable names to match user interface

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,45 +1,45 @@
 import { useState } from "react";
 
 export default function Body() {
-  const [x, setX] = useState("");
-  const [y, setY] = useState("");
   const [a, setA] = useState("");
   const [b, setB] = useState("");
+  const [c, setC] = useState("");
+  const [d, setD] = useState("");
   const [message, setMessage] = useState("");
 
   const handleCalculate = (e) => {
     e.preventDefault();
 
     if (
-      (!a && !b && !x && !y) ||
-      !a & !x ||
-      !a & !y ||
-      (!b && !x) ||
-      (!b && !y) ||
-      (!x && !y)
+      (!a && !b && !c && !d) ||
+      !c & !a ||
+      !c & !b ||
+      (!d && !a) ||
+      (!d && !b) ||
+      (!a && !b)
     ) {
       setMessage(`Please enter three inputs`);
-    } else if (!a && !b) {
-      const result = x / y;
+    } else if (!c && !d) {
+      const result = a / b;
       const percent = (result * 100).toFixed(2);
-      setMessage(`The ratio is ${x}:${y}, or ${percent}%.`);
-    } else if (!x) {
-      const result = y * (a / b);
+      setMessage(`The ratio is ${a}:${b}, or ${percent}%.`);
+    } else if (!a) {
+      const result = b * (c / d);
       if (Number.isNaN(result)) {
         setMessage("this works");
       }
       const fixedResult = result.toFixed(2);
       setMessage(`The missing unit is ${fixedResult}`);
-    } else if (!y) {
-      const result = (b * x) / a;
-      const fixedResult = result.toFixed(2);
-      setMessage(`The missing unit is ${fixedResult}`);
-    } else if (!a) {
-      const result = (b * x) / y;
-      const fixedResult = result.toFixed(2);
-      setMessage(`The missing unit is ${fixedResult}`);
     } else if (!b) {
-      const result = (a * y) / x;
+      const result = (d * a) / c;
+      const fixedResult = result.toFixed(2);
+      setMessage(`The missing unit is ${fixedResult}`);
+    } else if (!c) {
+      const result = (d * a) / b;
+      const fixedResult = result.toFixed(2);
+      setMessage(`The missing unit is ${fixedResult}`);
+    } else if (!d) {
+      const result = (c * b) / a;
       const fixedResult = result.toFixed(2);
       setMessage(`The missing unit is ${fixedResult}`);
     }
@@ -47,26 +47,27 @@ export default function Body() {
 
   return (
     <div className="main-container">
-      <h2>(Out of coffee) Ratio Calucator</h2>
-      <p>
-        Have you ever gone to make your morning coffee to then realize you don't
-        have enough beans for your typical brew? How many grams
-        <em> do </em>
-        you have? Great, but how many cups of water should you pour in? It's 6am
-        - too early for math.
-      </p>
-      <p>
-        In this case, enter the amount of grams you do have, and in the ratio
-        boxes, enter what you usually use. Hit calculate, and we'll tell you how
-        much water to use. This app is intended to fill in the missing link,
-        whatever it is.
-      </p>
-      <p>
-        Fill out 3 of 4 boxes to calculate your missing starting value, or fill
-        out the top two to find your ratio percentage.
-      </p>
-      <h3>A:B = C:D</h3>
-
+      <section>
+        <h2>(Out of coffee) Ratio Calucator</h2>
+        <p>
+          Have you ever gone to make your morning coffee to then realize you
+          don't have enough beans for your typical brew? How many grams
+          <em> do </em>
+          you have? Great, but how many cups of water should you pour in? It's
+          6am - too early for math.
+        </p>
+        <p>
+          In this case, enter the amount of grams you do have, and in the ratio
+          boxes, enter what you usually use. Hit calculate, and we'll tell you
+          how much water to use. This app is intended to fill in the missing
+          link, whatever it is.
+        </p>
+        <p>
+          Fill out 3 of 4 boxes to calculate your missing starting value, or
+          fill out the top two to find your ratio percentage.
+        </p>
+        <h3>A:B = C:D</h3>
+      </section>
       {message ? <h4>{message}</h4> : null}
       <form onSubmit={handleCalculate}>
         <div className="topInputContainer">
@@ -77,8 +78,8 @@ export default function Body() {
               step=".01"
               className="inputs"
               min="0"
-              value={x}
-              onChange={(e) => setX(parseFloat(e.target.value))}
+              value={a}
+              onChange={(e) => setA(parseFloat(e.target.value))}
             />
           </div>
           <div className="label-inputs">
@@ -88,8 +89,8 @@ export default function Body() {
               type="number"
               step=".01"
               min="0"
-              value={y}
-              onChange={(e) => setY(parseFloat(e.target.value))}
+              value={b}
+              onChange={(e) => setB(parseFloat(e.target.value))}
             />
           </div>
         </div>
@@ -102,8 +103,8 @@ export default function Body() {
               step=".01"
               className="ratio-inputs"
               min="0"
-              value={a}
-              onChange={(e) => setA(parseFloat(e.target.value))}
+              value={c}
+              onChange={(e) => setC(parseFloat(e.target.value))}
             />
           </div>
           <div className="label-inputs">
@@ -113,8 +114,8 @@ export default function Body() {
               step=".01"
               className="ratio-inputs"
               min="0"
-              value={b}
-              onChange={(e) => setB(parseFloat(e.target.value))}
+              value={d}
+              onChange={(e) => setD(parseFloat(e.target.value))}
             />
           </div>
         </div>
