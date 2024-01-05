19a4c026cf832dbb9527f013e9884268e1ac273d
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-05 16:04:27 -0500

    wrote unit tests for single missing values

--- _tests_/body.test.js
+++ _tests_/body.test.js
@@ -8,3 +8,15 @@ import {
 test("calculateMissingA determines the missing proportional value", () => {
   expect(calculateMissingA(4, 12, 16)).toBe("3.00");
 });
+
+test("calculateMissingB determines the missing proportional value", () => {
+  expect(calculateMissingA(3, 6, 12)).toBe("6.00");
+});
+
+test("calculateMissingC determines the missing proportional value", () => {
+  expect(calculateMissingA(7, 12, 16)).toBe("9.33");
+});
+
+test("calculateMissingD determines the missing proportional value", () => {
+  expect(calculateMissingA(3.2, 4.53, 16)).toBe("22.65");
+});
